89f52e35fdf7c5876406288c8c5e9bef28e5b36b
KimNam2809 <lekimnam2809@gmail.com>
2025-11-19 11:14:03 +0700

    update connect NextJS to FastAPI for profile 19/11/2025

--- WebsiteHoTroDichVuBanDoc_Backend/WebsiteHoTroDichVuBanDoc_Backend/app/models/nguoi_dung.py
+++ WebsiteHoTroDichVuBanDoc_Backend/WebsiteHoTroDichVuBanDoc_Backend/app/models/nguoi_dung.py
@@ -1,6 +1,6 @@
 from pydantic import EmailStr
 from typing import Optional
-from datetime import datetime
+from datetime import date, datetime
 from app.models.db_base import DBModel
 
 class NguoiDungBase(DBModel):
@@ -23,3 +23,23 @@ class NguoiDungUpdate(DBModel):
 class NguoiDung(NguoiDungBase):
     maNguoiDung: int
     ngayTao: Optional[datetime] = None
+
+class UserProfileResponse(DBModel):
+    # Thông tin chung
+    hoten: str
+    email: str
+    vaitro: str
+
+    # Thông tin Bạn Đọc (Optional - chỉ có nếu là Bạn đọc)
+    sothe: Optional[str] = None
+    tenthe: Optional[str] = None
+    ngayhethan: Optional[date] = None
+    trangthaithe: Optional[str] = None
+    tailieumuontoida: Optional[int] = 0
+
+    # Thông tin Nhân Viên (Optional - chỉ có nếu là Nhân viên)
+    manhanviennoibo: Optional[str] = None
+    phongban: Optional[str] = None
+    chucvu: Optional[str] = None
+    ngaytuyendung: Optional[date] = None
+
